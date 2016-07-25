99900557669292ef5d679405d794937417ee267c
Sergey Nikiforov <serge.a.nikiforov@gmail.com>
2016-07-25 23:16:52 +0300

    update files, external, communication & data setups

--- custom/setup-data.el
+++ custom/setup-data.el
@@ -1,1 +1,10 @@
 (provide 'setup-data)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; GROUP: Data -> Saveplace ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; saveplace remembers your location in a file when saving files
+(require 'saveplace)
+(toggle-save-place-globally 1)
+;; use (toggle-save-place-globally 1) in Emacs above 24.4
